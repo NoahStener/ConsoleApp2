96e41c2f31a4e3ed8abfa68b0b1c2248f4b0a062
Noah <Noah@STENER>
2022-09-21 09:46:05 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Var_Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var name = "Anas";
+            //var num = 10;
+
+            //int 1 = name.Length; //Length funkar ej för int 
+
+
+
+            //dynamic nam1 = "Anas";
+            ////int 12 = name1.length;
+            //dynamic num2 = 10;
+        }
+    }
+}
